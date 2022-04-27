a3368ca9e3180fd631ec622b1a2a866d7b71bfa6
kgiun1 <kgiun1@naver.com>
2022-04-27 16:56:15 +0900

    Create-developer(POST) 구현 및 테스트

---
+++ src/main/java/com/example/dmaker/controller/DMakerController.java
@@ -0,0 +1,37 @@
+package com.example.dmaker.controller;
+
+import com.example.dmaker.dto.CreateDeveloper;
+import com.example.dmaker.service.DMakerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class DMakerController {
+
+    private final DMakerService dMakerService;
+
+    @GetMapping("/developers")
+    public List<String> getAllDevelopers() {
+        log.info("GET /developers HTTP/1.1");
+
+        return Arrays.asList("snow", "Elsa", "Olaf");
+    }
+
+    @PostMapping("/create-developer")
+    public CreateDeveloper.Response createDeveloper(@Valid @RequestBody CreateDeveloper.Request request) {
+        log.info("request : {}", request);
+
+        return dMakerService.createDeveloper(request);
+    }
+}
--- src/main/java/com/example/dmaker/service/DMakerService.java
+++ src/main/java/com/example/dmaker/service/DMakerService.java
@@ -1,6 +1,8 @@
 package com.example.dmaker.service;
 
+import com.example.dmaker.dto.CreateDeveloper;
 import com.example.dmaker.entity.Developer;
+import com.example.dmaker.exception.DMakerException;
 import com.example.dmaker.repository.DeveloperRepository;
 import com.example.dmaker.type.DeveloperLevel;
 import com.example.dmaker.type.DeveloperSkillLevel;
@@ -9,21 +11,48 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+import static com.example.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
+
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
 
     @Transactional
-    public void createDeveloper(){
+    public CreateDeveloper.Response createDeveloper(CreateDeveloper.Request request) {
+        validateCreateDeveloperRequest(request);
+
         Developer developer = Developer.builder()
-                .develoerLevel(DeveloperLevel.JUNIOR)
-                .develoerSkillLevel(DeveloperSkillLevel.FRONT_END)
-                .experienceYears(2)
-                .name("Olaf")
-                .age(5)
+                .develoerLevel(request.getDeveloperLevel())
+                .develoerSkillLevel(request.getDeveloperSkillLevel())
+                .experienceYears(request.getExperienceYears())
+                .memberId(request.getMemberId())
+                .name(request.getName())
+                .age(request.getAge())
                 .build();
 
         developerRepository.save(developer);
+        return new CreateDeveloper.Response().fromEntity(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        developerRepository.findByMemberId(request.getMemberId())
+                .ifPresent((developer -> {
+                    throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+                }));
     }
 }
